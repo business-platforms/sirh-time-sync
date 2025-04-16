ac8aa00992c180d9ac89e69d1cf29a4afa06ff48
aimed <aimededdine.touati@tech-instinct.com>
2025-04-16 12:12:01 +0100

    refact(main_window): update resource_path to use meipass

--- src/ui/main_window.py
+++ src/ui/main_window.py
@@ -365,8 +365,8 @@ class MainWindow:
         """Get absolute path to resource, works for dev and for PyInstaller."""
         try:
             # PyInstaller creates a temp folder and stores path in _MEIPASS
-            base_path = sys._MEIPASS
-        except AttributeError:
+            base_path = getattr(sys, '_MEIPASS', os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+        except Exception:
             base_path = os.path.abspath(".")
 
         return os.path.join(base_path, relative_path)
